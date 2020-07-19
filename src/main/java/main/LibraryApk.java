c41fd20881a229119342c2ac3b1f0c1f2e7f42c0
DaDudek <dawiddudekstg@gmail.com>
2020-07-19 19:28:38 +0200

    init some start values

---
+++ src/main/java/main/LibraryApk.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LibraryApk extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane mainPane = FXMLLoader.load(getClass().getResource("/mainPane.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
+        stage.setTitle("Library Apk");
+        stage.show();
+
+    }
+}

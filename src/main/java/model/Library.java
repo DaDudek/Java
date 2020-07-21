373921d1fb832a799f7f44480f2953283c50ab07
DaDudek <dawiddudekstg@gmail.com>
2020-07-21 18:07:57 +0200

    added saveChanges method

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -1,10 +1,11 @@
 package model;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Library implements Serializable {
+    public final static String fileName = "Library.obj";
     private Map<Integer, Publication> publicationMap= new HashMap<>();
 
     public Map<Integer, Publication> getPublicationMap() {
@@ -22,7 +23,15 @@ public class Library implements Serializable {
         publicationMap.put(publication.getId(), publication);
     }
 
-    
+    public void saveChanges(){
+        try(    FileOutputStream fos = new FileOutputStream(fileName);
+                ObjectOutputStream ous = new ObjectOutputStream(fos) )
+        {
+            ous.writeObject(publicationMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }

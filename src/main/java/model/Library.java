13ec61ef79717be0a32bb5e376d826a0d8ffab57
DaDudek <dawiddudekstg@gmail.com>
2020-07-21 18:18:09 +0200

    remove wrong method

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -6,32 +6,24 @@ import java.util.Map;
 
 public class Library implements Serializable {
     public final static String fileName = "Library.obj";
-    private Map<Integer, Publication> publicationMap= new HashMap<>();
+    private Map<Integer, Publication> publicationsMap = new HashMap<>();
+    private Map<String, Publication> usersMap = new HashMap<>();
 
-    public Map<Integer, Publication> getPublicationMap() {
-        return publicationMap;
+    public Map<Integer, Publication> getPublicationsMap() {
+        return publicationsMap;
     }
 
     public void setPublicationMap(Map<Integer, Publication> publicationMap) {
-        this.publicationMap = publicationMap;
+        this.publicationsMap = publicationMap;
     }
 
     public Library() {
     }
 
     public void addPublication(Publication publication){
-        publicationMap.put(publication.getId(), publication);
+        publicationsMap.put(publication.getId(), publication);
     }
 
-    public void saveChanges(){
-        try(    FileOutputStream fos = new FileOutputStream(fileName);
-                ObjectOutputStream ous = new ObjectOutputStream(fos) )
-        {
-            ous.writeObject(publicationMap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 
 }

99ad64bc648614e33af6be5b73410ccd01587624
Dawid Dudek <dawiddudekstg@gmail.com>
2020-07-26 17:57:00 +0200

    added errorPane and skeleton of controller

---
+++ src/main/java/controllers/ErrorPaneController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ErrorPaneController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Label errorMessageLabel;
+
+}
